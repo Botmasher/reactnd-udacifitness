983c9db3eeccde4416690e265ff4250867b5f1e2
Botmasher <languagejosh@hotmail.com>
2017-12-07 10:23:49 -0800

    feat: Add tab navigation and status bar - React Native - lesson 4 section 2

--- App.js
+++ App.js
@@ -5,13 +5,62 @@ import History from './components/History';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import reducer from './reducers';
+import { TabNavigator } from 'react-navigation';
+import { purple, white } from '../utils/colors';
+import { FontAwesome, Ionicons } from '@expo/vector-icons';
+import { StatusBar } from 'react-native';
+import { Constants } from 'expo';
+
+function UdaciStatusBar({ backgroundColor, ...props }) {
+  return (
+    <View style={{backgroundColor, height: Constants.statusBarHeight}}>
+      <StatusBar translucent backgroundColor={backgroundColor} {...props} />
+    </View>
+  );
+}
+
+const Tabs =  TabNavigator({
+  History: {
+    screen: History,
+    navigationOptions: {
+      tabBarLabel: 'History',
+      tabBarIcon: ({ tintColor }) => <Ionicons name="ios-bookmarks" size={30} color={tintColor} />
+    }
+  },
+  AddEntry: {
+    screen: AddEntry
+    navigationOptions: {
+      tabBarLabel: 'AddEntry',
+      tabBarIcon: ({ tintColor }) => <FontAwesome name="plus-square" size={30} color={tintColor} />
+    }
+  }
+}, {
+  navigationOptions: {
+    header: null
+  },
+  tabBarOptions: {
+    activeTintColor: Platform.OS === 'ios' ? purple : white,
+    style: {
+      height: 56,
+      backgroundColor: Platform.OS === 'ios' ? white : purple,
+      shadowColor: 'rgb(0, 0, 0, 0.24)',
+      shadowOffset: {
+        width: 0,
+        height: 3
+      },
+      shadowRadius: 6,
+      shadowOpacity: 3
+    }
+  }
+});
 
 export default class App extends React.Component {
   render() {
     return (
       <Provider store={createStore(reducer)}>
         <View style={styles.container}>
-          <AddEntry />
+          <UdaciStatusBar backgroundColor={purple} barStyle="light-content" />
+          <Tabs />
         </View>
       </Provider>
     );
